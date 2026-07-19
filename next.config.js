9d0c6f73f677639033aabeed067c189b4ca08314
balkingrep <161081802+balkingrep@users.noreply.github.com>
2026-07-19 05:57:57 +0530

    fix: ensure typescript ignoreBuildErrors is set

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
+  typescript: { ignoreBuildErrors: true, tsconfigPath: "./tsconfig.json" },
   reactStrictMode: true,
   async headers() {
     return [
